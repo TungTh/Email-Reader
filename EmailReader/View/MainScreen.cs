a6e502d45d6dda5fdf6ea7b258efbe867393b9df
unknown <Administrator@.(none)>
2010-03-08 14:29:52 +0700

    Them giao dien chinh#	new file:   EmailReader/View/MainScreen.Designer.cs

---
+++ EmailReader/View/MainScreen.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EmailReader.View
+{
+    public partial class MainScreen : Form
+    {
+        public MainScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
