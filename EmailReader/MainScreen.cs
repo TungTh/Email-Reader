a649c4a19c2a4ccfa4ea035afed8baab38035710
HaiNH <nguyen_hong_hai@yahoo.com>
2010-03-16 08:46:39 +0700

    Hai commit lai cho chac

---
+++ EmailReader/MainScreen.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EmailReader
+{
+    public partial class MainScreen : Form
+    {
+        public MainScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void tagToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
